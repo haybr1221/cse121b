5bd7b02347ab4ce2c9f84fb602d0a319bfab0976
haybr1221 <hbranchflower@gmail.com>
2024-01-20 16:58:51 -0700

    w02 tasks

---
+++ sandbox/w02-practice.js
@@ -0,0 +1,38 @@
+// Write a Javascript snippet to do each of the following. 
+// Make your best guess if you don't know. 
+// Make sure to put some sort of response on each:
+
+let x = 5
+let y = 2
+let a = "H"
+let b = "i"
+
+// Add two numbers
+z = x + y
+
+// Multiply two numbers
+z = x * y
+
+// Subtract one from a number
+z = x - y
+
+// concatinate two strings together.
+z = a + b
+
+// assign a value to a variable
+let c = 1
+
+// increment the value in a variable by 3
+c = c + 3
+
+// compare two values to see if they are the same
+x == y
+
+// check to see if one number is less than another number
+x < y
+
+// Check to see if two values are NOT equal
+x != y
+
+// check to see if a value is less than 10 and greater than 0
+0 < x < 10
